82d442bb78c445ae4ad5bc9965643d1cf8f0386d
Prabha (Intern) <prabha@Polestarllp.local>
2024-08-21 17:18:22 +0530

    my-app

---
+++ src/components/menuApi.js
@@ -0,0 +1,120 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "Maggi",
+    category: "breakfast",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 2,
+    image: "images/pizza.jpg",
+    name: "Pizza",
+    category: "lunch",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 3,
+    image: "images/chole-bhature.webp",
+    name: "Chole-bhature",
+    category: "lunch",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 4,
+    image: "images/momo.jpg",
+    name: "Momo",
+    category: "evening",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 5,
+    image: "images/Dal-makhani.jpg",
+    name: "Dal-Makhni",
+    category: "dinner",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 6,
+    image: "images/Gol-Gappe.jpg",
+    name: "Gol-Gappe",
+    category: "evening",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 7,
+    image: "images/burger.webp",
+    name: "Burger",
+    category: "evening",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 8,
+    image: "images/paneer-tikka.jpg",
+    name: "Paneer-Tikka",
+    category: "lunch",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 9,
+    image: "images/rajma-chawal.jpg",
+    name: "Rajma-Chawal",
+    category: "dinner",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 10,
+    image: "images/pasta.jpg",
+    name: "Pasta",
+    category: "breakfast",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 11,
+    image: "images/poha.jpg",
+    name: "Poha",
+    category: "dinner",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 12,
+    image: "images/Chai-pakoda.jpg",
+    name: "Chai-Pakoda",
+    category: "breakfast",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+  {
+    id: 12,
+    image: "images/Chai-pakoda.jpg",
+    name: "Chai-Pakoda",
+    category: "anytime",
+    price: 20,
+    description:
+      "maggi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur atque vero eius error rem, modi neque odio ipsam voluptas aliquid, non nobis consequuntur quibusdam eligendi ab reiciendis mollitia doloribus eaque",
+  },
+];
+export default Menu;
